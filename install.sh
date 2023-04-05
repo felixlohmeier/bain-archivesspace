3f5ea34e1665790556d754db138ab9108e2c8659
Felix Lohmeier <mail@felixlohmeier.de>
2023-04-05 13:13:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
- wget -q https://github.com/archivesspace/archivesspace/releases/download/v3.3.1/archivesspace-v3.3.1.zip
- unzip -q archivesspace-v3.3.1.zip
+ wget -q https://github.com/archivesspace/archivesspace/releases/download/v3.1.1/archivesspace-v3.1.1.zip
+ unzip -q archivesspace-v3.1.1.zip
